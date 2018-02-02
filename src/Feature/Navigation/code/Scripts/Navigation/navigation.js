c851ab744b84a3208fbbb78bac2a4917e12157df
Marenych Igor <marenych.igor@gmail.com>
2018-02-02 15:19:08 +0200

    Refactored navigation.

---
+++ src/Feature/Navigation/code/Scripts/Navigation/navigation.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+
+function toogleItem(e, level) {
+    var selector = `.js-child-${level}`;
+
+    var parentNode = e.target.parentNode;
+    var childElement = parentNode.querySelectorAll(selector);
+
+    childElement.forEach(function (child) {
+        child.classList.toggle("d-none");
+    });
+}
